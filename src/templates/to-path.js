972bc6f817303dd52f60b129949d55e046df6fa7
gdhardy1 <gdhardy1@gmail.com>
2021-09-01 08:59:18 -0400

    feat: show pathname

--- src/templates/to-path.js
+++ src/templates/to-path.js
@@ -2,12 +2,16 @@ import * as React from "react"
 
 const ToPath = ({ pageContext }) => {
   const { fromPath, toPath, isPermanent } = pageContext
+  const isBrowser = typeof window !== undefined
 
   return (
     <>
       <h1>{`fromPath: ${fromPath}`}</h1>
       <h1>{`toPath: ${toPath}`}</h1>
       <h1>{`isPermanent: ${isPermanent ? isPermanent : false}`}</h1>
+      <h1>{`window.location.pathname: ${
+        isBrowser && window.location.pathname
+      }`}</h1>
     </>
   )
 }
